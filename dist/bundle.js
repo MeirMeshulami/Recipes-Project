7c4557e098373a93a63e224b15421023f8c8d1b0
MeirMeshulami <meir.meshoulami@gmail.com>
2024-01-14 16:32:08 +0200

    fix bundle.js

--- dist/bundle.js
+++ dist/bundle.js
@@ -5,7 +5,7 @@
  * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
+ *///
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
